c8a3fafa9057b3f17664e2799f5609a9c3f00f4f
canteuf <juniorjteuf@gmail.com>
2023-05-24 01:59:26 +0100

    Création de l'arborescence

---
+++ Electroshop/includes/nav.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <style>
+            body {
+                box-sizing: border-box;
+                padding: 0;
+                margin: 0;
+            }
+
+            .navbar {
+                display: flex;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li> <i class="fa-solid fa-house"></i> <a href="#">Accueil</a></li>
+
+                <li> <i class="fa-solid fa-mobile"></i> <a href="#">Téléphones</a>
+                    <div class="sous_menu">
+                        
+                        <ul>
+                            <h2>OS</h2>
+                            <li><a href="#">Android</a></li>
+                            <li><a href="#">Apple</a></li>
+                        </ul>
+                        <ul>
+                            <h2>Marques</h2>  
+                            <li><a href="#">Samsung</a></li>
+                            <li><a href="#">Huawei</a></li>
+                            <li><a href="#">Xiaomi</a></li>
+                            <li><a href="#">Google</a></li>
+                            <li><a href="#">Tecno</a></li>
+                        </ul>
+
+                        <ul>
+                            <h2>Accessoires</h2>                           
+                            <li><a href="#">Smartwatch</a></li>
+                            <li><a href="#">Pochettes et Glaces</a></li>
+                            <li><a href="#">Power Bank </a></li>
+                            <li><a href="#">Ecouteurs et Chargeur</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li> <i class="fa-solid fa-gamepad"></i> <a href="#">Gaming et Jeux Vidéos</a>
+                    <div class="sous_menu">
+                        
+                        <ul>
+                            <h2>Consoles</h2>
+                            <li><a href="#">Playstation</a></li>
+                            <li><a href="#">Xbox</a></li>
+                            <li><a href="#">Nintendo</a></li>
+                        </ul>
+
+                        <ul>
+                            <h2>Gaming PC</h2>
+                            <li><a href="#">Laptop</a></li>
+                            <li><a href="#">Desktop</a></li>
+                            <li><a href="#">Accessoires</a></li>
+                        </ul>
+                    </div>
+                
+                </li>
+
+                <li><i class="fa-solid fa-blender-phone"></i> <a href="#">Electromenager</a>
+                    <div class="sous_menu">
+                        <ul>
+                            <h2>Cuisine</h2>
+                            <li><a href="#">Appareils de Cuisson</a></li>
+                            <li><a href="#">Blender et Hachoir</a></li>                         
+                        </ul>
+                        <ul>
+                            <h2>Appareils de ménage</h2>
+                            <li><a href="#">Fer à repasser</a></li>
+                            <li><a href="#">Aspirateur</a></li>
+                        </ul>
+                    </div>
+
+                </li>
+                <li> <i class="fa-solid fa-laptop"></i> <a href="#">Informatique</a>
+                    <div class="sous_menu">
+                        <ul>
+                            <h2>Ordinateurs</h2>
+                            <li><a href="#">Laptop</a></li>
+                            <li><a href="#">Desktop</a></li>
+                            <li><a href="#">Tablette PC</a></li>
+                        </ul>
+
+                        <ul>
+                            <h2>Périphériques</h2>
+                            <li><a href="#">Clavier et Souris</a></li>
+                            <li><a href="#">Alimentations PC</a></li>
+                            <li><a href="#">Moniteurs</a></li>
+                        </ul>
+
+                        <ul>
+                            <h2>Stockage</h2>
+                            <li><a href="#">Disques Durs</a></li>
+                            <li><a href="#">Clés USB</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>        
+    </body>
+</html>
